aaa59212d357d3fdfac2cc5c0f98ec2ba6922004
zackscovitch01 <bagoudoumoubarack@icloud.com>
2022-02-01 21:04:36 -0500

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,21 +24,20 @@ const Navbar = () => {
         <a href="/" className="p__opensans">Book Table</a>
       </div>
       <div className="app__navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick= {() => setToggleMenu(true)}/>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick= {() => setToggleMenu(true)} />
         
-        {toggleMenu && (
-          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onclick= {() => setToggleMenu(false)}/>
-            <ul className="app__navbar-smallscreen-links">
-              <li className="p__opensans"><a href="#home">Home</a></li>
-              <li className="p__opensans"><a href="#about">About</a></li>
-              <li className="p__opensans"><a href="#menu">Menu</a></li>
-              <li className="p__opensans"><a href="#awards">Home</a></li>
-              <li className="p__opensans"><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-          )
-        }
+        {toggleMenu &&
+            (<div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick= {() => setToggleMenu(false)}/>
+              <ul className="app__navbar-smallscreen_links">
+                <li className="p__opensans"><a href="#home">Home</a></li>
+                <li className="p__opensans"><a href="#about">About</a></li>
+                <li className="p__opensans"><a href="#menu">Menu</a></li>
+                <li className="p__opensans"><a href="#awards">Home</a></li>
+                <li className="p__opensans"><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          )}
       </div>
     </nav>
 )
